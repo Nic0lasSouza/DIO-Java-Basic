16bde0d4d03e9161f6c98abbd98aa8728e88ca14
Nicolas Souza <NicolasdsmdSouza2004@gmail.com>
2022-12-27 22:22:13 -0300

    add

---
+++ meu-primeiro-projeto-java/src/MinhaClasse.java
@@ -0,0 +1,12 @@
+public class MinhaClasse {
+    public static void main(String[] args) {
+        String primeiroNome = "Nicolas";
+        String segundoNome= "Souza";
+        String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+
+        System.out.println(nomeCompleto);
+    }
+    public static String nomeCompleto (String primeiroNome, String segundoNome){
+        return "Resultado do método:" + primeiroNome.concat(" ").concat(segundoNome);
+    }
+}
